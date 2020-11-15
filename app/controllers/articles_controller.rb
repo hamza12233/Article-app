877c8ba981e9d918ce9a96d1755bc64f76f5ddcd
Hamza Saleem <hafizhamzasaleem4@gmail.com>
2020-11-15 15:22:18 +0500

    create update delete and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -14,8 +18,28 @@ class ArticlesController < ApplicationController
      end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully edited"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 
